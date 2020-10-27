324e5fb36d8fdb010206e2a95d6b95f2253c09b3
Marcelo Santos <rocha.mer21@gmail.com>
2020-10-27 14:51:06 -0300

    testando o Persistency

--- src/dip/services/persistency.spec.ts
+++ src/dip/services/persistency.spec.ts
@@ -1,13 +1,25 @@
-describe('TESTANDO ALGUMA COISA', () => {
-  it('descrição do teste (IT)', () => {
-    const number = 1
-    expect(number).toBe(1)
-  })
-})
+import { Persistency } from './persistency'
 
-describe('TESTANDO OUTRA COISA', () => {
-  test('should return Marcelo', () => {
-    const name = 'Marcelo'
-    expect(name).toBe('Marcelo')
+describe('Persistency', () => {
+  afterEach(() => jest.clearAllMocks())
+
+  it('should return undefined', () => {
+    // sut = System under test
+    const sut = new Persistency()
+    expect(sut.saveOrder()).toBeUndefined()
+  })
+
+  it('should call console.log once', () => {
+    const sut = new Persistency()
+    const consoleSpy = jest.spyOn(console, 'log')
+    sut.saveOrder()
+    expect(consoleSpy).toHaveBeenCalledTimes(1)
+  })
+
+  it('should call console.log with "Pedido salvo com sucesso..."', () => {
+    const sut = new Persistency()
+    const consoleSpy = jest.spyOn(console, 'log')
+    sut.saveOrder()
+    expect(consoleSpy).toHaveBeenCalledWith('Pedido salvo com sucesso...')
   })
 })
