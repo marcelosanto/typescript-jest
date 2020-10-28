ae2a583277cee783bc8b05ebf816da1c9c42066f
Marcelo Santos <rocha.mer21@gmail.com>
2020-10-28 01:35:48 -0400

    product

---
+++ src/dip/classes/product.spec.ts
@@ -0,0 +1,16 @@
+import { Product } from './product'
+
+const createSut = (name: string, price: number): Product => {
+  return new Product(name, price)
+}
+
+describe('Product', () => {
+  afterEach(() => jest.clearAllMocks())
+
+  it('should return undefined', () => {
+    const sut = createSut('Sapato', 50.99)
+
+    expect(sut).toHaveProperty('name', 'Sapato')
+    expect(sut.price).toBeCloseTo(50.99)
+  })
+})
