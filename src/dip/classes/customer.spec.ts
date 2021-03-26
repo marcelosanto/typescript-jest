2b9039b59af09cf88f4540763edb90c2d6e8ac12
Marcelo Santos <rocha.mer21@gmail.com>
2021-03-26 09:13:37 -0300

    feat: test for customer

--- src/dip/classes/customer.spec.ts
+++ src/dip/classes/customer.spec.ts
@@ -33,15 +33,15 @@ describe('IndividualCustomer', () => {
 })
 
 describe('EnterpriseCustomer', () => {
-  it('should have name, cnpj.', () => {
+  it('should have name and cnpj.', () => {
     const sut = createEnterpriseCustomer('Mrs Labs', '125.168.989-25/0001')
-    expect(sut).toHaveProperty('name', 'Marcelo')
-    expect(sut).toHaveProperty('cnpj', '125.168.989-25')
+    expect(sut).toHaveProperty('name', 'Mrs Labs')
+    expect(sut).toHaveProperty('cnpj', '125.168.989-25/0001')
   })
 
-  it('should have firstName, lastName, cpf', () => {
+  it('should have methods to get name and idn for enterprise customers', () => {
     const sut = createEnterpriseCustomer('Mrs Labs', '125.168.989-25/0001')
-    expect(sut.getName()).toBe('Marcelo Santos')
-    expect(sut.getIDN()).toBe('125.168.989-25')
+    expect(sut.getName()).toBe('Mrs Labs')
+    expect(sut.getIDN()).toBe('125.168.989-25/0001')
   })
 })
